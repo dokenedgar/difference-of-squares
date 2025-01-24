aacc194ff2c93554cad4a4aca6657ff2a4df3147
Doken Edgar <dokenedgarjr@gmail.com>
2025-01-24 01:51:47 +0100

    Add solution for square of sum

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    todo!("square of sum of 1...{n}")
+    let mut counter = 1;
+    let mut total: u32 = 0;
+    loop {
+        if counter <= n {
+            total += counter;
+            counter += 1;
+        } else {
+            break;
+        }
+    }
+    total = total.pow(2);
+    total
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
@@ -7,5 +18,7 @@ pub fn sum_of_squares(n: u32) -> u32 {
 }
 
 pub fn difference(n: u32) -> u32 {
-    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    // todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    let diff_result = square_of_sum(n);
+    diff_result
 }
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let res = difference_of_squares::difference(10);
+    println!("Res: {}", res)
+}
